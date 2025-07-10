7db4d06fe526f8161e1899ba9f4e237f971ffc1c
Peric Marko <marko.peric@eurosystem.it>
2025-07-10 12:59:59 +0200

    aggiunti campi al parser

--- parsers/cowrie_parser.py
+++ parsers/cowrie_parser.py
@@ -11,24 +11,29 @@ class CowrieParser(InterfaceLogParser):
 
     Questo parser trasforma una stringa JSON in un dizionario standardizzato
     utile per l'analisi di eventi di attacco e attività sospette, estraendo i campi rilevanti
-
-    
     '''
+
     def parse(self, raw_log: str) -> dict[str, Any]:
         try:
             log_data = json.loads(raw_log)
             return {
                 "timestamp": log_data.get("timestamp"),
                 "src_ip": log_data.get("src_ip"),
-                #"src_port": log_data.get("src_port"),
+                "src_port": log_data.get("src_port"),
+                "dst_ip": log_data.get("dst_ip"),
                 "dst_port": log_data.get("dst_port"),
                 "event": log_data.get("eventid"),
                 "message": log_data.get("message"),
                 "username": log_data.get("username"),
                 "password": log_data.get("password"),
-                "command": log_data.get("command"),
+                "command": log_data.get("command") or log_data.get("input"),
                 "session": log_data.get("session"),
-                "protocol": log_data.get("protocol")
+                "protocol": log_data.get("protocol"),
+                "ssh_version": log_data.get("version"),
+                "hassh": log_data.get("hassh"),
+                "ttylog": log_data.get("ttylog"),
+                "duration": log_data.get("duration"),
+                "sensor": log_data.get("sensor"),
             }
         except json.JSONDecodeError:
             logger.error(f"Failed to parse log: {raw_log}")
